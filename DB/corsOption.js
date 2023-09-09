0fe15c38e8956fb7f6e71575dd621de3e86c81d8
gregorymark254 <gregorymark254@gmail.com>
2023-09-09 16:41:25 +0300

    api

--- DB/corsOption.js
+++ DB/corsOption.js
@@ -2,8 +2,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:3002',
-    'https://kilimanihub.vercel.app/'
+    'https://kilimanihub.vercel.app'
 ]
 
 const corsOption = {
