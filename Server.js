b744c5b7b9c22cd1a10019243dde7a026b2435e6
gregorymark254 <gregorymark254@gmail.com>
2023-09-09 14:07:46 +0300

    kilimani server

---
+++ Server.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const corsOption = require("./DB/corsOption")
+const mongoconnect = require("./DB/MongoDb")
+const {logger } = require("./Middleware/logEvents")
+const errorHandler = require("./Middleware/errorHandler")
+
+//connection to Databases
+mongoconnect()
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors(corsOption))
+app.use(logger)
+
+//routes
+app.get("/", (req, res) => {
+    res.json({Message:"Kilimani Hub Backend Server."});
+});
+
+//Error handler
+app.use(errorHandler)
+
+
+//Connetion to the server
+const PORT = process.env.PORT 
+const server = app.listen(PORT, () => console.log(`Server is running on port:${PORT}`))
