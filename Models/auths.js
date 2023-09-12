8ab1d47e873361699ea71538e6fd1aef5ebafdd3
gregorymark254 <gregorymark254@gmail.com>
2023-09-12 11:59:01 +0300

    auth

--- Models/auths.js
+++ Models/auths.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema(
         email : { type:String, required:true,unique:true },
         phone : { type:String, required:true,unique:true },
         password : { type:String, required:true },
-        isAdmin: { type: Boolean, default: false, required: true }
+        isAdmin: { type: String, default: 'User' }
     },
     { timestamps : true }
 )
