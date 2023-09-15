d942527126c850503b36ffbb8c088be51b51c20f
gregorymark254 <gregorymark254@gmail.com>
2023-09-16 02:09:24 +0300

    update

--- Models/vote.js
+++ Models/vote.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema(
     {
-        votequestion : { type:String, required:true,unique:false }
+        votequestion : { type:String, required:true,unique:false },
         text : { type:String, required:true,unique:false }
     },
     { timestamps : true }
